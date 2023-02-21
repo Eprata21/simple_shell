8fb8194498d166cf434f5dec069ecb08d5940cd3
Noahxyzs <105595182+Noahxyzs@users.noreply.github.com>
2023-02-21 18:40:56 +0300

    Shell is very important in unix

--- memory.c
+++ memory.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * bfree - frees a pointer and NULLs the address
+ * bfree - free a pointer and NULLs the address
  * @ptr: address of the pointer to free
  *
  * Return: 1 if freed, otherwise 0.
