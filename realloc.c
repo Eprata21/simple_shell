12938f82c7d4871b7106e50ce0cf87f421a7e75c
Noahxyzs <105595182+Noahxyzs@users.noreply.github.com>
2023-02-21 18:44:49 +0300

    Importance of shell

--- realloc.c
+++ realloc.c
@@ -4,7 +4,7 @@
  * _memset - fills memory with a constant byte
  * @s: the pointer to the memory area
  * @b: the byte to fill *s with
- * @n: the amount of bytes to be filled
+ * @n: the amount of bytes to be fill
  * Return: (s) a pointer to the memory area s
  */
 char *_memset(char *s, char b, unsigned int n)
